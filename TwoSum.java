8ac5f83f434db66e236e4062050f58e7379429bc
Rishab0323 <srishbh574@gmail.com>
2025-02-19 14:50:38 +0530

    problem 1

---
+++ TwoSum.java
@@ -0,0 +1,20 @@
+"leecode problem name Two sum"
+
+Class TwoSum{
+    public static void main(String [] args){
+    int nums[]={2,7,11,15};
+    int target = 9;
+    System.out.println(twoSum(nums,target));
+
+    }
+    public int[] twoSum(int[] nums, int target) {
+     for(int i=0;i<nums.length-1;i++){
+        for(int j=i+1;j<nums.length;j++){
+        if(nums[i]+nums[j]==target){
+            return new int[]{i,j};
+         }
+        }   
+    }
+    return new int[]{-1};
+}
+}
