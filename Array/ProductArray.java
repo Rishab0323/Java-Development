e744827f394f9c358b7bb1cccb1f68a5db3570d3
Rishab0323 <srishbh574@gmail.com>
2025-03-10 23:57:38 +0530

    prob 5

--- Array/ProductArray.java
+++ Array/ProductArray.java
@@ -1,8 +1,8 @@
 class ProductArray{
     public static void main(String [] args){
         int arr[] = {1,2,3,4};
-        productExcpetSelf(arr);
-        for(int ele: arr){
+        int hel[] =productExcpetSelf(arr);
+        for(int ele: hel){
             System.out.print(ele +" ");
         }
 
@@ -27,5 +27,27 @@ class ProductArray{
         res[i] = prod;
     }
      return res;
+    }
+
+    //optiised approach using prefix and suffix
+
+    public int[] productExceptSelf(int[] nums) {
+        int prefix[] = new int[nums.length];
+        int suffix[] = new int[nums.length];
+        int[] ans= new int[nums.length];
+        prefix[0] = 1;
+        suffix[nums.length-1] = 1;
+        for(int i=1;i<nums.length;i++){
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+
+        for(int i=nums.length-2;i>=0;i--){
+            suffix[i] = suffix[i+1] *nums[i+1];
+        }
+
+        for(int i=0;i<nums.length;i++){
+            ans[i] = prefix[i] * suffix[i];        
+            }
+      return ans;
     }
 }
