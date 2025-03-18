3d30873ff6dc21b72a1084fc4a214a90311500d2
Rishab0323 <srishbh574@gmail.com>
2025-03-18 23:56:45 +0530

    prob 6

--- Array/MaxSubarray.java
+++ Array/MaxSubarray.java
@@ -1,20 +1,39 @@
 class MaxSubarray{
     public static void main(String[] args){
         int arr[] ={2, 3, -8, 7, -1, 2, 3};
-        System.out.println(maxSum(arr));
+        //.out.println(maxSum(arr));
+        System.out.println(maxSubArray(arr));
     }
 
-    public static int maxSum(int arr[]){
-        int max = 0;
+//brute force approach
+//    public static int maxSum(int arr[]){
+//         int max = 0;
+//         for(int i=0;i<arr.length;i++){
+//             int currSum = 0;
+//             for(int j=i;j<arr.length;j++){
+//                 currSum += arr[j];
+//             }
+//             if(currSum > max){
+//                 max = currSum;
+//             }
+//         }
+//         return max;   
+//     }
+
+    //optimised approach kadane algorithm
+    public static int maxSubArray(int[] arr) {
+        int max_sum = 0;
+        int cur_sum = 0;
         for(int i=0;i<arr.length;i++){
-            int currSum = 0;
-            for(int j=i;j<arr.length;j++){
-                currSum += arr[j];
+            cur_sum += arr[i];
+            if(cur_sum < 0){
+                cur_sum = 0;
             }
-            if(currSum > max){
-                max = currSum;
+            
+            if(cur_sum > max_sum){
+            max_sum = cur_sum;
             }
         }
-        return max;   
+        return max_sum;
     }
 }
