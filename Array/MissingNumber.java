72eefc874e012e0d50fa76776962f99f803bf4cd
Rishab0323 <srishbh574@gmail.com>
2025-08-03 23:42:09 +0530

    optimized solution

--- Array/MissingNumber.java
+++ Array/MissingNumber.java
@@ -17,5 +17,18 @@ class MissingNumber {
             }
         }
         return missingValue;
+
+        //optimized approach
+
+        public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int totalSum = (n * (n + 1)/2);
+        int partialSum = 0;
+        for(int i=0;i<n;i++){
+            partialSum += nums[i];
+        }
+        int missingValue = totalSum - partialSum;
+        return missingValue;
+    }
     }
 }
