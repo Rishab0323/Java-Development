5c02292fcf47f47a5199b22b40ee2fdb2d7dd50a
Rishab0323 <srishbh574@gmail.com>
2025-08-05 23:36:33 +0530

    leetcode task done

---
+++ Array/ProductExecptOf.java
@@ -0,0 +1,24 @@
+class  ProductExecptOf{
+    public int[] productExceptSelf(int[] nums) {
+        int product [] = new int[nums.length];
+        int i = 0;
+        int j = 0;
+        product[i] = 1;
+        while(i < nums.length){
+            if(j == nums.length){
+                i++;
+                if(i < nums.length){
+                    product[i] = 1;
+                    j = 0;
+                }
+                else{
+                    if( i != j ){
+                        product[i] *= nums[j];
+                    }
+                    j++;
+                }
+            }
+        }
+        return product;
+    }
+}
