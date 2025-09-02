09faa05fb67a4ff7878b98f30c070bc7d3bd8fd0
Rishab0323 <srishbh574@gmail.com>
2025-09-02 23:54:06 +0530

    peak element dsa

---
+++ searching/PeakElement.java
@@ -0,0 +1,22 @@
+class PeakElement{
+    public static void main(String[] args){
+        int nums[] = {1,2,3,1};
+        int start = 0;
+        int end = nums.length -1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid] < nums[mid-1]){
+                end = mid - 1;
+            }
+            else if(nums[mid] < nums[mid+1]){
+                start = mid + 1;
+            }
+        } 
+        return -1;
+    }
+}
