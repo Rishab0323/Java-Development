e3048b4a62209b33f749bb63bebf7097b8281169
Rishab0323 <srishbh574@gmail.com>
2025-10-05 21:15:59 +0530

    revising tree

---
+++ Trees/PracTree.java
@@ -0,0 +1,25 @@
+class PracTree{
+
+    private Node root;
+    
+    private class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+    }
+
+    public insertNode(int value){
+        
+    }
+
+    public static void main(String[] args){
+        PracTree tree = new PracTree();
+        System.out.println(tree.root);
+    }
+
+}
