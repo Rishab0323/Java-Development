abda341926eff88a649820e6501f32a576cc1788
Rishab0323 <srishbh574@gmail.com>
2025-10-06 11:32:41 +0530

    hands on code

--- Trees/PracTree.java
+++ Trees/PracTree.java
@@ -1,4 +1,4 @@
-class PracTree{
+class PrTree{
 
     private Node root;
     
@@ -13,13 +13,48 @@ class PracTree{
 
     }
 
-    public insertNode(int value){
+    private Node insert(Node node,int value){
+        if(node == null){
+            Node newNode = new Node(value);
+            return newNode;
+        }
+
+        if(value < node.value){
+            node.left = insert(node.left,value);
+        }
+
+        if(value > node.value){
+            node.right = insert(node.right,value);
+        }
+        return node;
+    }
+
+
+    public void insertSorted(int value){
+        root = insert(root,value);
+
+    }
+
+    public void display(){
+        if(root == null){
+            System.out.println("root is empty" +root);
+            return;
+        }
+        System.out.println(root.value);
+        return;
         
     }
-
-    public static void main(String[] args){
-        PracTree tree = new PracTree();
-        System.out.println(tree.root);
-    }
-
 }
+
+class PracTree{
+    public static void main(String [] args){
+        PrTree tree = new PrTree();
+        tree.display();
+
+        // int nums[] = {1,2,3,4,5,6,7,8,9};
+        int num = 12;
+        tree.insertSorted(num);
+        tree.display();
+        
+    }
+    }
