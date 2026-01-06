11b10e3abadb3abb87713395cba9a3508ffc13d3
Rishab0323 <srishbh574@gmail.com>
2026-01-06 08:11:10 +0530

    problem on bfs

--- Trees/Practise_tree.java
+++ Trees/Practise_tree.java
@@ -38,6 +38,7 @@ class Practise_tree{
     Node getValue(){
         return value;
     }
+//leetcode problem 102
 
 public List<List<Integer>> levelOrder(TreeNode root){
     List<List<Intger>> result = new ArrayList<>();
@@ -67,7 +68,36 @@ public List<List<Integer>> levelOrder(TreeNode root){
     return result;
 }
 
-}
+//leetcode problem 637
+public List<Double> averageOfLevels(Node root){
+    List<Double> avg = new ArrayList<>();
+    
+    if(root == null){
+        return avg;
+    }
+    
+    Queue<Node> queue = new ArrayList<>();
+    queue.offer(root);
+
+    while(!queue.isEmpty()){
+        int levelSize = queue.size();
+        double averagelevels = 0;
+        for(int i=0;i<levelSize;i++){
+            Node currentNode = queue.poll();
+            averagelevels += currentLevel.val;
+            if(curentNode.left != null){
+                queue.offer(curentNode.left);
+            }
+            if(curentNode.right != null){
+                queue.offer(curentNode.right);
+            }
+            int currentAvg = averagelevels/levelsize;
+            avg.add(currentAvg);
+        }
+    return avg;
+    }
+} 
+
 
 class Practise_tree{
     public static void main(String [] args){
