f606a5b91892f34f21f46ede90d8d959dd37d331
Rishab0323 <srishbh574@gmail.com>
2026-01-07 08:43:25 +0530

    problem on bfs

--- Trees/Practise_tree.java
+++ Trees/Practise_tree.java
@@ -140,6 +140,31 @@ public List<Double> averageOfLevels(Node root){
         }
     return result;
     }
+
+
+    //leetcode problem 116
+
+    public Node connect(Node root) {
+   
+        if(root == null){
+            return null;
+        }
+        Node leftMost = root;
+
+        while(leftMost.left != null){
+        Node current = leftMost;
+        while(current != null ){
+            current.left.next = current.right;
+            if(current.next != null ){
+            current.right.next = current.next.left;
+        }
+        current = current.next;
+        }
+        leftMost = leftMost.left;
+    }
+    return root;
+}
+
 } 
 
 
